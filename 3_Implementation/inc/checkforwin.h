6a9e064360d84a9f5a16555f8ef277158d7afb2a
jrnjco <94299490+jrnjco@users.noreply.github.com>
2021-11-25 09:27:39 +0530

    Create checkforwin.h

---
+++ 3_Implementation/inc/checkforwin.h
@@ -0,0 +1,18 @@
+/** 
+ *
+* @file checkforwin.h
+* checking on the players current symbol and adding the points
+*
+*/
+#ifndef __CHECKFORWIN_H_
+#define  _CHECKFORWIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void board();
+void rules();
+int checkforwin();
+
+#endif
